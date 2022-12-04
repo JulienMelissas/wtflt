d792e8c52839dd2af5f74b4b7cec41d8ce5169c0
Julien Melissas <julien@arryved.com>
2022-12-04 11:05:38 -0600

    Handle a POST from the form to /artist/

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,14 @@
+import { Form } from "@remix-run/react";
+
 export default function Index() {
   return (
     <>
       <h1>Put a fucking artist in:</h1>
+      <Form method="post" action="/artist" reloadDocument>
+        <label htmlFor="artist">Search</label>
+        <input name="artist" type="text" />
+        <button type="submit">Let's fucking go.</button>
+      </Form>
     </>
   );
 }

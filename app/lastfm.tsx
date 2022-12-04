60ac6aea0af162654eeb557b3170b86360dc11c1
Julien Melissas <julien@arryved.com>
2022-12-03 19:09:42 -0600

    Loading similar artists from the last.fm API Also added support for env vars (CF method)

---
+++ app/lastfm.tsx
@@ -0,0 +1,15 @@
+// @todo - figure out how to get this here
+// const LAST_FM_API_KEY = context.LAST_FM_API_KEY;
+const API_BASE = 'http://ws.audioscrobbler.com/2.0/';
+
+/**
+ * Gets Similar Artists
+ * 
+ * @param artist 
+ * @param apiKey 
+ * @returns
+ */
+export async function getSimilarArtists( artist: string, apiKey: string ) {
+  const response = fetch(`${API_BASE}?method=artist.getsimilar&artist=${artist}&api_key=${apiKey}&format=json`).then()
+  return (await response).json();
+}
--- app/routes/artist/$artist.tsx
+++ app/routes/artist/$artist.tsx
@@ -1,12 +1,22 @@
 import { json } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 
-export const loader = async ({ params }) => {
-  return json({ artist: params.artist });
+import { getSimilarArtists } from "~/lastfm";
+
+export const loader = async ({ context, params }: { params: any, context: any }) => {
+  const LAST_FM_API_KEY = context.LAST_FM_API_KEY;
+  const similarArtists = await getSimilarArtists(params.artist, LAST_FM_API_KEY);
+
+  return json({
+    artist: params.artist,
+    similarArtists: similarArtists,
+  });
 };
 
 export default function Artist() {
-  const { artist } = useLoaderData();
+  const { artist, similarArtists } = useLoaderData();
+
+  console.log(artist, similarArtists);
   
   return (
     <>

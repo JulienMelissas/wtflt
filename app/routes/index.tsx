754a66abf9c439e1d1a518b70c17648bf0d343d1
Julien Melissas <julien@arryved.com>
2022-12-04 22:36:36 -0600

    Added an sr-only class, search cleanup

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,8 +5,8 @@ export default function Index() {
     <>
       <h1>Put a fucking artist in:</h1>
       <Form method="get" action="/recommend" reloadDocument>
-        <label htmlFor="artist">Search</label>
-        <input name="artist" type="text" />
+        <label className="sr-only" htmlFor="artist-input">Enter an artist you like:</label>
+        <input id="artist-input" name="artist" type="text" />
         <button className="small" type="submit">Let's fucking go.</button>
       </Form>
       <Link className="small" to="/lucky">Fuck it, I'm feeling lucky.</Link>

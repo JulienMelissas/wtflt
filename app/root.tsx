f539a03c48cd7e5f6755d13c025a8a5a7a48459e
Julien Melissas <julien@arryved.com>
2022-12-08 23:23:04 -0600

    Added GA code

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,15 @@ export default function App() {
       <head>
         <Meta />
         <Links />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-X239JYDTQV"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-X239JYDTQV');
+        </script>
       </head>
       <body>
         <div className="wrap">
